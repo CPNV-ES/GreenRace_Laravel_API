1adf7f52e5dc0b6508d0dfa74014744df701bb7f
Fabio <fabio-manuel.silva-marques@cpnv.ch>
2018-02-09 10:59:49 +0100

    create Vehicle controller

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Vehicle;
+
+class VehicleController extends Controller
+{
+  public function index(){
+    return Vehicle::all();
+  }
+
+  public function show(Vehicle $vehicle){
+    return $vehicle;
+  }
+
+  public function store(Request $request){
+    $vehicle = Vehicle::create($request->all());
+
+    return response()->json($vehicle, 201);
+  }
+
+  public function update(Request $request, Vehicle $vehicle){
+    $vehicle->update($request->all());
+
+    return response()->json($vehicle, 200);
+  }
+
+  public function delete(Vehicle $vehicle){
+    $vehicle->delete();
+
+    return response()->json(null, 204);
+  }
+}
