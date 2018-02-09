bfee418f95ab29a08b609cfa9c92d22d8ddafcd8
Fabio <fabio-manuel.silva-marques@cpnv.ch>
2018-02-09 11:49:41 +0100

    Passport config

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * API Authentification provider
+         */
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
