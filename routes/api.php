ddd4c31279b33b51023ffa5d0d77cb2a69c6805b
Fabio <fabio-manuel.silva-marques@cpnv.ch>
2018-02-09 10:16:47 +0100

    created routes for trips, categories, vehicles

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Categories routes
 // get all
 Route::get('categories', 'CategoryController@index');
 // get by id
@@ -27,3 +28,27 @@ Route::post('category', 'CategoryController@store');
 Route::put('category/{category}', 'CategoryController@update');
 // delete
 Route::delete('category/{category}', 'CategoryController@delete');
+
+// Trips routes
+// get all
+Route::get('trips', 'TripController@index');
+// get by id
+Route::get('trip/{trip}', 'TripController@show');
+// create
+Route::post('trip', 'TripController@store');
+// modify
+Route::put('trip/{trip}', 'TripController@update');
+// delete
+Route::delete('trip/{trip}', 'TripController@delete');
+
+// Vehicle routes
+// get all
+Route::get('vehicles', 'VehicleController@index');
+// get by id
+Route::get('vehicle/{vehicle}', 'VehicleController@show');
+// create
+Route::post('vehicle', 'VehicleController@store');
+// modify
+Route::put('vehicle/{vehicle}', 'VehicleController@update');
+// delete
+Route::delete('vehicle/{vehicle}', 'VehicleController@delete');
